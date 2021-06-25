1e651339d8c371d2f920cb357fa0ae3adfd41c0a
Cỏ Độc <hungln006@gmail.com>
2021-06-26 01:52:32 +0700

    img

--- photo.js
+++ photo.js
@@ -1,7 +1,7 @@
 let gallery = document.getElementsByClassName('gallery-icon');
         for (const key of gallery) {
            let a = key.childNodes[1];
-           let img = a.childNodes[1];
+           let img = a.childNodes[0];
          //   let namephoto = img.src.split('/')[8].split('.')[0];
          //   a.href = `https://lengochung.github.io/wordpress/${namephoto}`;
            console.log(img);
