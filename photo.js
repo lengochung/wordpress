59fcb6233ca503bd11eb352f05f7b4fcdc5ca028
Cỏ Độc <hungln006@gmail.com>
2021-06-26 01:30:41 +0700

    sua index mang thanh 3

--- photo.js
+++ photo.js
@@ -1,6 +1,6 @@
 let gallery = document.getElementsByClassName('gallery-icon');
         for (const key of gallery) {
-           let a = key.childNodes[1];
+           let a = key.childNodes[3];
            let img = a.childNodes[1];
            let namephoto = img.src.split('/')[8].split('.')[0];
            a.href = `https://lengochung.github.io/wordpress/${namephoto}`;
