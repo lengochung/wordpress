e21a1b94ac34d373621aed0df1d49b745356672f
Cỏ Độc <hungln006@gmail.com>
2021-06-26 01:20:46 +0700

    them file js photo.js trong thu muc san pham

---
+++ san-pham/photo.js
@@ -0,0 +1,8 @@
+let gallery = document.getElementsByClassName('gallery-icon');
+        for (const key of gallery) {
+           let a = key.childNodes[1];
+           let img = a.childNodes[1];
+           let namephoto = img.src.split('/')[8].split('.')[0];
+           a.href = `https://lengochung.github.io/wordpress/${namephoto}`;
+           console.log(a.href);
+        }
