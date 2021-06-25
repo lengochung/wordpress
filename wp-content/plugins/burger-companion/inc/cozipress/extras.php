cadf88b34a3b0181034a655c5a799d8f526338dc
Cỏ Độc <hungln006@gmail.com>
2021-06-25 23:18:45 +0700

    them tren window

---
+++ wp-content/plugins/burger-companion/inc/cozipress/extras.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function add_action() in C:\xampp\htdocs\myproject\wordpress\wp-content\plugins\burger-companion\inc\cozipress\extras.php:27
+Stack trace:
+#0 {main}
+  thrown in <b>C:\xampp\htdocs\myproject\wordpress\wp-content\plugins\burger-companion\inc\cozipress\extras.php</b> on line <b>27</b><br>
