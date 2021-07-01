10b3003a5834ef1ed6a52a905e183a9d46c63de2
Cỏ Độc <hungln006@gmail.com>
2021-07-01 11:49:16 +0700

    xoa 23

--- wp-content/plugins/burger-companion/inc/cozipress/default-widgets/default-widget.php
+++
@@ -1,5 +0,0 @@
-<br>
-<b>Fatal error</b>:  Uncaught Error: Call to undefined function wp_get_theme() in C:\xampp\htdocs\myproject\wordpress\wp-content\plugins\burger-companion\inc\cozipress\default-widgets\default-widget.php:2
-Stack trace:
-#0 {main}
-  thrown in <b>C:\xampp\htdocs\myproject\wordpress\wp-content\plugins\burger-companion\inc\cozipress\default-widgets\default-widget.php</b> on line <b>2</b><br>
--- wp-content/plugins/burger-companion/inc/spintech/sections/section-slider.php
+++
@@ -1,7 +0,0 @@
- <!--===// Start: Slider
-    =================================--> 
-<br>
-<b>Fatal error</b>:  Uncaught Error: Call to undefined function apply_filters() in C:\xampp\htdocs\myproject\wordpress\wp-content\plugins\burger-companion\inc\spintech\sections\section-slider.php:67
-Stack trace:
-#0 {main}
-  thrown in <b>C:\xampp\htdocs\myproject\wordpress\wp-content\plugins\burger-companion\inc\spintech\sections\section-slider.php</b> on line <b>67</b><br>
