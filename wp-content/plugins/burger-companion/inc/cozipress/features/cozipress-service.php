7d398dbfda8190849c342d92483ac31de14e5661
Cỏ Độc <hungln006@gmail.com>
2021-07-02 22:52:34 +0700

    them 25

---
+++ wp-content/plugins/burger-companion/inc/cozipress/features/cozipress-service.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function add_action() in C:\xampp\htdocs\myproject\wordpress\wp-content\plugins\burger-companion\inc\cozipress\features\cozipress-service.php:224
+Stack trace:
+#0 {main}
+  thrown in <b>C:\xampp\htdocs\myproject\wordpress\wp-content\plugins\burger-companion\inc\cozipress\features\cozipress-service.php</b> on line <b>224</b><br>
---
+++ wp-content/plugins/burger-companion/inc/spintech/features/spintech-typography.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function add_action() in C:\xampp\htdocs\myproject\wordpress\wp-content\plugins\burger-companion\inc\spintech\features\spintech-typography.php:261
+Stack trace:
+#0 {main}
+  thrown in <b>C:\xampp\htdocs\myproject\wordpress\wp-content\plugins\burger-companion\inc\spintech\features\spintech-typography.php</b> on line <b>261</b><br>
