b6c44fd6aa15f51252d7a53b36469d6ddf3a3a95
Cỏ Độc <hungln006@gmail.com>
2021-07-02 21:52:30 +0700

    xoa 24

--- wp-content/plugins/burger-companion/inc/spintech/dynamic-style.php
+++
@@ -1,5 +0,0 @@
-<br>
-<b>Fatal error</b>:  Uncaught Error: Call to undefined function add_action() in C:\xampp\htdocs\myproject\wordpress\wp-content\plugins\burger-companion\inc\spintech\dynamic-style.php:76
-Stack trace:
-#0 {main}
-  thrown in <b>C:\xampp\htdocs\myproject\wordpress\wp-content\plugins\burger-companion\inc\spintech\dynamic-style.php</b> on line <b>76</b><br>
--- wp-content/plugins/burger-companion/inc/spintech/features/spintech-above-header.php
+++
@@ -1,5 +0,0 @@
-<br>
-<b>Fatal error</b>:  Uncaught Error: Call to undefined function add_action() in C:\xampp\htdocs\myproject\wordpress\wp-content\plugins\burger-companion\inc\spintech\features\spintech-above-header.php:302
-Stack trace:
-#0 {main}
-  thrown in <b>C:\xampp\htdocs\myproject\wordpress\wp-content\plugins\burger-companion\inc\spintech\features\spintech-above-header.php</b> on line <b>302</b><br>
