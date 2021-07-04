634886c80067196ca048653c06e2030ca43ba2af
Cỏ Độc <hungln006@gmail.com>
2021-07-05 03:22:04 +0700

    xoa 26

--- wp-content/plugins/burger-companion/inc/cozipress/sections/section-slider.php
+++
@@ -1,7 +0,0 @@
- <!--===// Start: Slider
-    =================================--> 
-<br>
-<b>Fatal error</b>:  Uncaught Error: Call to undefined function apply_filters() in C:\xampp\htdocs\myproject\wordpress\wp-content\plugins\burger-companion\inc\cozipress\sections\section-slider.php:61
-Stack trace:
-#0 {main}
-  thrown in <b>C:\xampp\htdocs\myproject\wordpress\wp-content\plugins\burger-companion\inc\cozipress\sections\section-slider.php</b> on line <b>61</b><br>
--- wp-content/plugins/burger-companion/inc/spintech/sections/section-cta.php
+++
@@ -1,5 +0,0 @@
-<br>
-<b>Fatal error</b>:  Uncaught Error: Call to undefined function apply_filters() in C:\xampp\htdocs\myproject\wordpress\wp-content\plugins\burger-companion\inc\spintech\sections\section-cta.php:52
-Stack trace:
-#0 {main}
-  thrown in <b>C:\xampp\htdocs\myproject\wordpress\wp-content\plugins\burger-companion\inc\spintech\sections\section-cta.php</b> on line <b>52</b><br>
