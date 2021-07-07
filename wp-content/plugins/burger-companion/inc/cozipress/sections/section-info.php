52c4a4787009375ea74b95a93f9107ffb49e989a
Cỏ Độc <hungln006@gmail.com>
2021-07-07 14:17:34 +0700

    xóa 27

--- wp-content/plugins/burger-companion/inc/cozipress/sections/section-info.php
+++
@@ -1,5 +0,0 @@
-<br>
-<b>Fatal error</b>:  Uncaught Error: Call to undefined function apply_filters() in C:\xampp\htdocs\myproject\wordpress\wp-content\plugins\burger-companion\inc\cozipress\sections\section-info.php:91
-Stack trace:
-#0 {main}
-  thrown in <b>C:\xampp\htdocs\myproject\wordpress\wp-content\plugins\burger-companion\inc\cozipress\sections\section-info.php</b> on line <b>91</b><br>
--- wp-content/plugins/burger-companion/inc/spintech/features/spintech-slider.php
+++
@@ -1,5 +0,0 @@
-<br>
-<b>Fatal error</b>:  Uncaught Error: Call to undefined function add_action() in C:\xampp\htdocs\myproject\wordpress\wp-content\plugins\burger-companion\inc\spintech\features\spintech-slider.php:105
-Stack trace:
-#0 {main}
-  thrown in <b>C:\xampp\htdocs\myproject\wordpress\wp-content\plugins\burger-companion\inc\spintech\features\spintech-slider.php</b> on line <b>105</b><br>
